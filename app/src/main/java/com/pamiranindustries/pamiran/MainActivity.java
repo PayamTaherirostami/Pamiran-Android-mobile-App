91756682c2b710e7aa8cb17dc77febccff824946
Payam Taherirostami <payamtaherirostami@gmail.com>
2021-05-23 12:15:33 -0700

    temp commit

--- app/src/main/java/com/pamiranindustries/pamiran/MainActivity.java
+++ app/src/main/java/com/pamiranindustries/pamiran/MainActivity.java
@@ -2,9 +2,23 @@
 package com.pamiranindustries.pamiran;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -14,20 +28,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
+
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,52 +39,66 @@ import android.widget.Toolbar;
 
 import java.text.BreakIterator;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button0;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button buttonLogin;
+    FirebaseAuth mAuth;
+    EditText editTextEmail, editTextPassword, editTextUserName;
+    ProgressBar progressBar;
+    private EditText username;
     private String[] myStringArray={"Movies","Cameras","Map","Service 4","Service 5","Service 6"};
-    //    int position;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        mAuth = FirebaseAuth.getInstance();
         GridView gridView = (GridView) findViewById(R.id.gridView);
-        BtnAdapter adapter= new BtnAdapter(MainActivity.this,myStringArray);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextUserName= (EditText) findViewById(R.id.editTextUserName);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        buttonLogin= (Button) findViewById(R.id.buttonLogin);
 
+        BtnAdapter adapter= new BtnAdapter(MainActivity.this,myStringArray);
         gridView.setAdapter(new BtnAdapter(this,myStringArray));
         gridView.setAdapter(adapter);
 
+        findViewById(R.id.textViewSignup).setOnClickListener(this);
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+
 
     }
+
+
     public void OtherActivity(View view) {
 //        startActivity(new Intent(MainActivity.this,MoviesActivity.class));\
 //        Toast.makeText(MainActivity.this,"will work soon!",Toast.LENGTH_LONG).show();
-        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-        mBuilder.setIcon(android.R.drawable.sym_def_app_icon);
-        mBuilder.setTitle("About us");
-        mBuilder.setMessage("Hello this is us");
-        mBuilder.setCancelable(false);
-        mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int which) {
-                dialogInterface.dismiss();
-            }
-        });
-        mBuilder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int which) {
-                Toast.makeText(MainActivity.this,"will work soon!",Toast.LENGTH_LONG).show();
-            }
-        });
-        mBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startActivity(new Intent(MainActivity.this,MoviesActivity.class));
-            }
-        });
-        AlertDialog alertDialog = mBuilder.create();
-        alertDialog.show();
+//        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+//        mBuilder.setIcon(android.R.drawable.sym_def_app_icon);
+//        mBuilder.setTitle("About us");
+//        mBuilder.setMessage("Hello this is us");
+//        mBuilder.setCancelable(false);
+//        mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int which) {
+//                dialogInterface.dismiss();
+//            }
+//        });
+//        mBuilder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int which) {
+//                Toast.makeText(MainActivity.this,"will work soon!",Toast.LENGTH_LONG).show();
+//            }
+//        });
+//        mBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                startActivity(new Intent(MainActivity.this,MoviesActivity.class));
+//            }
+//        });
+//        AlertDialog alertDialog = mBuilder.create();
+//        alertDialog.show();
     }
 
     @Override
@@ -112,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
     private class BtnOnClickListener implements View.OnClickListener{
 
         @Override
@@ -131,9 +150,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
-
 
     public class BtnAdapter extends BaseAdapter {
 
@@ -177,6 +193,110 @@ public class MainActivity extends AppCompatActivity {
             return btn;
         }
 
+    }
+//    private void userLogin() {
+    private void signIn(){
+
+        Log.d("FIREBASE", "signIn");
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String username = editTextUserName.getText().toString().trim();
+        Log.d("FIREBASE", "email:" + email);
+        Log.d("FIREBASE", "password:" + password);
+//        if (username.isEmpty()) {
+//            editTextUserName.setError("UserName is required");
+//            editTextUserName.requestFocus();
+//            return;
+//        }
+//
+//        if (email.isEmpty()) {
+//            editTextEmail.setError("Email is required");
+//            editTextEmail.requestFocus();
+//            return;
+//        }
+//
+//        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            editTextEmail.setError("Please enter a valid email");
+//            editTextEmail.requestFocus();
+//            return;
+//        }
+//
+//        if (password.isEmpty()) {
+//            editTextPassword.setError("Password is required");
+//            editTextPassword.requestFocus();
+//            return;
+//        }
+//
+//        if (password.length() < 6) {
+//            editTextPassword.setError("Minimum lenght of password should be 6");
+//            editTextPassword.requestFocus();
+//            return;
+//        }
+
+//        progressBar.setVisibility(View.VISIBLE);
+
+//        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+//                progressBar.setVisibility(View.GONE);
+                Log.d("FIREBASE", "signIn:onComplete:" + task.isSuccessful());
+
+                if (task.isSuccessful()) {
+                    // update profile
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                            .setDisplayName(username)
+                            .build();
+
+                    user.updateProfile(profileUpdates)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("FIREBASE", "User profile updated.");
+                                        // Go to FirebaseActivity
+                                        startActivity(new Intent(MainActivity.this, MoviesActivity.class));
+                                    }
+                                }
+                            });
+
+                } else {
+                    Log.d("FIREBASE", "sign-in failed");
+
+                    Toast.makeText(MainActivity.this, "Sign In Failed",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+//                if (task.isSuccessful()) {
+//                    finish();
+//                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    startActivity(intent);
+//                } else {
+//                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
+   @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+//            case R.id.textViewSignup:
+//                finish();
+//                startActivity(new Intent(this, SignUpActivity.class));
+//                break;
+
+            case R.id.buttonLogin:
+//                userLogin();
+                signIn();
+                break;
+        }
     }
 
 }
---
+++ app/src/main/java/com/pamiranindustries/pamiran/User.java
@@ -0,0 +1,18 @@
+package com.pamiranindustries.pamiran;
+
+public class User {
+
+    String firstName, lastName, age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
